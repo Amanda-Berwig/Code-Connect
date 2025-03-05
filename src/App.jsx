55e6d7982857df1c54504a88eaacf73012716b05
Amanda Berwig <amandaberwig@gmail.com>
2025-03-05 16:05:46 -0300

    initialize React app with Vite, add main components and styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./assets/components/Sidebar";
+import BarraDePesquisa from "./assets/components/BarraDePesquisa";
+
+function App() {
+  return (
+    <div className="container">
+      <Sidebar />
+      <BarraDePesquisa />
+    </div>
+  );
+}
+
+export default App;
